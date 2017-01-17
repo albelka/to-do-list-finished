b84e31141057e825d6891091e5177aea94ffdf8f
Steve-Burton <stevevburton@gmail.com>
2017-01-17 13:46:38 -0800

    add form to edit to do list, with 2-way data binding

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,21 @@ import { Component } from '@angular/core';
     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
     <h3>{{currentFocus}}</h3>
     <ul>
-      <li (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask()">Edit</button></li>
+      <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit</button></li>
     </ul>
+    <hr>
+    <div>
+      <h3>{{selectedTask.description}}</h3>
+      <p>Task complete? {{selectedTask.done}}</p>
+      <h3>Edit task</h3>
+      <label>Enter task description:</label>
+      <input [(ngModel)]="selectedTask.description">
+      <label> Enter task priority (1-3):</label>
+      <br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium priority)<br>
+      <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High priority)<br>
+    </div>
   </div>
   `
 })
@@ -20,13 +33,25 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task("Finish weekend Angular homework for Epicodus course"),
-    new Task("Begin brainstorming possible JavaScript group projects"),
-    new Task("Add README file to last few Angular repos on GitHub")
+    new Task("Finish weekend Angular homework for Epicodus course", 3),
+    new Task("Begin brainstorming possible JavaScript group projects", 2),
+    new Task("Add README file to last few Angular repos on GitHub", 1)
   ];
 
-  editTask() {
-    alert("You just requested to edit a Task");
+  priorityColor(currentTask) {
+    if (currentTask.priority === 3) {
+      return "bg-danger";
+    } else if (currentTask.priority === 2) {
+      return "bg-warning";
+    } else {
+      return "bg-info";
+    }
+  }
+
+  selectedTask: Task = this.tasks[0];
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
   }
 
   isDone(clickedTask: Task) {
@@ -40,5 +65,5 @@ export class AppComponent {
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) {}
+  constructor(public description: string, public priority: number) {}
 }
